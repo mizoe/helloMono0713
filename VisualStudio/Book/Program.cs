51481fccbdc102dca9dfa0e0b1de114b90f21063
Tomo Mizoe <mz@july.co.jp>
2015-07-16 12:31:03 +0900

    Jul16

---
+++ VisualStudio/Book/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book();
+            book1.title = "C#入門";
+            book1.publishedDate = new DateTime(2000, 12, 31);
+            book1.author = "七月太郎";
+            book1.price = 1000;
+
+            int taxIncludedPrice = book1.getTaxIncludedPrice();
+            int tax = book1.getTax();
+
+            Console.WriteLine(tax + " " + taxIncludedPrice);
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+
+            var p1 = new Person();
+            p1.lastName = "田中";
+            p1.firstName = "太郎";
+            p1.furiganaLastName = "たなか";
+            p1.furiganaFirstName = "たろう";
+            p1.birthday = new DateTime(2009,1,1);
+
+            Console.Write(p1.lastName + " " + p1.firstName);
+            Console.Write("(" + p1.furiganaLastName + " " 
+                + p1.furiganaFirstName + ")、");
+            Console.WriteLine(p1.birthday.Year 
+                + "/" + p1.birthday.Month
+                + "/" + p1.birthday.Day + "生 " + p1.getAge() +"才");
+
+            var p2 = new Person();
+            p2.lastName = "鈴木";
+            p2.firstName = "花子";
+            p2.furiganaLastName = "すずき";
+            p2.furiganaFirstName = "はなこ";
+            p2.birthday = new DateTime(1998, 12, 31);
+
+            Console.Write(p2.lastName + " " + p2.firstName);
+            Console.Write("(" + p2.furiganaLastName + " "
+                + p2.furiganaFirstName + ")、");
+            Console.WriteLine(p2.birthday.Year
+                + "/" + p2.birthday.Month
+                + "/" + p2.birthday.Day + "生 " + p2.getAge() + "才");
+
+        }
+    }
+
+    class Book
+    {
+        public string title;
+        public string author;
+        public DateTime publishedDate;
+        public int price;
+
+        public const double taxRatio = 0.08;
+
+        public int getTax()
+        {
+            return (int)(price * taxRatio);
+        }
+
+        public int getTaxIncludedPrice()
+        {
+            return price + getTax();
+        }
+    }
+
+    class Person
+    {
+        public string lastName;
+        public string firstName;
+        public string furiganaLastName;
+        public string furiganaFirstName;
+        public DateTime birthday;
+
+        public int getAge()
+        {
+            int age = 0;
+            DateTime now = DateTime.Now;
+            TimeSpan ts = now - birthday;
+            age = (int)((double)ts.Days / 365.2425);
+            return age;
+        } 
+    }
+}
---
+++ VisualStudio/p2_5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> scores = new Dictionary<string, int>();
+            scores["矢吹"] = 2;
+            scores["丹下"] = 74;
+            scores["力石"] = 80;
+            scores["白木"] = 100;
+            scores["西"] = 10;
+            int total = 0;
+
+            foreach (var key in scores.Keys)
+            {
+                var name = key;
+                //氏名：矢吹　点数：  2
+                if (key.Length == 1)
+                {
+                    name = "　" + key;
+                }
+                var rank = "不可";
+                int jadge = scores[key] / 10;
+                switch (jadge)
+                {
+                    case 10:
+                    case 9:
+                    case 8:
+                        rank = "優";
+                        break;
+                    case 7:
+                        rank = "良";
+                        break;
+                    case 6:
+                        rank = "可";
+                        break;
+                }
+                Console.WriteLine("氏名：{0} 点数：{1,3} 評価：{2}", name, scores[key], rank);
+                total += scores[key];
+
+            }
+
+            Console.WriteLine("受験者\t{0,3}名", scores.Count);
+            Console.WriteLine("合計点\t{0,3}点", total);
+            Console.WriteLine("平均点\t{0,3}点", total / scores.Count);
+        }
+    }
+}
